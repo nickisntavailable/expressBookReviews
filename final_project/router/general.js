d808ff93b6c76a8be16639af5ab59a11041aad18
Daniil Kolesnikov <kolda10.97@gmail.com>
2026-09-02 12:51:14 -0400

    task3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,8 +25,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const result = Object.keys(books).filter(key => books[key].author === author).map(key => books[key]);
+  res.send(result);
+//   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
