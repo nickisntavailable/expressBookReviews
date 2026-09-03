24da7b65ff78b57361fb2d27142a6d5bd7bed61b
Daniil Kolesnikov <kolda10.97@gmail.com>
2026-09-03 05:00:57 -0400

    taks9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -69,6 +69,28 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 //   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+
+    if (!books[isbn]) {
+        return res.status(404).json({
+            message: 'Book not found.'
+        });
+    }
+
+    if (!books[isbn].reviews[username]) {
+        return res.status(404).json({
+            message: 'Review not found.'
+        });
+    }
+
+    delete books[isbn].reviews[username];
+    return res.status(200).json({
+        message: 'Review deleted.'
+    });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
